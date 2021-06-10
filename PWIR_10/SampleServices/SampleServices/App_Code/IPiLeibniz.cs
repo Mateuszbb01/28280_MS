8480ab0897bec71b1586fa1c0662f34023e5622f
Mateuszbb01 <57901484+Mateuszbb01@users.noreply.github.com>
2021-06-10 12:34:43 +0200

    Add files via upload

---
+++ PWIR_10/SampleServices/SampleServices/App_Code/IPiLeibniz.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+
+// UWAGA:możesz użyć polecenia „Zmień nazwę” w menu „Refaktoryzuj”, aby zmienić nazwę interfejsu „IPiLeibniz” w kodzie i pliku konfiguracji.
+[ServiceContract]
+public interface IPiLeibniz
+{
+    [OperationContract]
+    void DoWork();
+
+    [OperationContract]
+    double CalculatePi(int accuracy);
+}
